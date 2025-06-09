f1b232fa62eb73a905504bd971f41fe9a41142ef
권용준 <yoyo91828h@gmail.com>
2025-06-09 20:08:47 +0900

    레벨 1-3: Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,7 +1,6 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,15 +10,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserChangePasswordRequest {
 
-    // Bean Validation 이용
-    @NotBlank(message = "기존 비밀번호는 필수입니다.")
+    // Validation 이용
+    @NotBlank
     private String oldPassword;
 
-    @NotBlank(message = "새 비밀번호는 필수입니다.")
-    @Pattern(
-            regexp = "^(?=.*[A-Z])(?=.*\\d).{8,}$",
-            message = "새 비밀번호는 8자 이상이어야 하며, 숫자와 대문자를 포함해야 합니다."
-    )
+    @NotBlank
     private String newPassword;
 }
 
